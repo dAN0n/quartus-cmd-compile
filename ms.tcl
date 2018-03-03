125eb4f39a3c061cdca979525160587f05451fc6
Zobkov Dmitry <dan0na@ya.ru>
2018-03-03 03:33:55 +0300

    Minor refactoring

--- ms.tcl
+++ ms.tcl
@@ -1,16 +1,5 @@
-set files   [string trim $2]
-set dofiles [string trim $3]
-
-project open $1
-foreach file $files { project addfile ./src/$file }
-foreach file $dofiles { project addfile ./src/$file }
-project calculateorder
-
-foreach file $files { vlog ./src/$file }
-vsim work.$1 -wlf ./src/$1.wlf
-
-if { [file exists ./src/$1.do] == 0 } {
-	set tcl [open ./src/$1.do w]
+proc generate_do {name} {
+	set tcl [open ./src/$name.do w]
 	puts $tcl "# Tutorial for generate waveform in tcl"
 	puts $tcl "# From: https://www.microsemi.com/document-portal/doc_view/136364-modelsim-me-10-4c-command-reference-manual-for-libero-soc-v11-7"
 	puts $tcl "#"
@@ -21,7 +10,7 @@ if { [file exists ./src/$1.do] == 0 } {
 	puts $tcl "# For example: \"virtual signal {sw7_4 {sw\[7:4\]}}\""
 	puts $tcl "#"
 	puts $tcl "# Use \"force <name> <value> \[@\]<time>\" for set signal value for some relative time (@ - absolute time)"
-	puts $tcl "# For example \"force sw\[1:0\] 2'b10 10, 2#01 5\""
+	puts $tcl "# For example \"force sw\[1:0\] 2'b10 10, 2#01 20\""
 	puts $tcl "#"
 	puts $tcl "# \"-repeat (-r) \[@\]<time>\" flag repeats a series of forced values and times at the time specified"
 	puts $tcl "# \"-cancel \[@\]<time>\" flag cancels the force command at the time specified"
@@ -39,6 +28,21 @@ if { [file exists ./src/$1.do] == 0 } {
 	puts $tcl "#"
 	puts $tcl "# For zooming you can use \"zoom full\" or \"zoom range <start> <end>\""
 	close $tcl
+}
+
+set files   [string trim $2]
+set dofiles [string trim $3]
+
+project open $1
+foreach file $files { project addfile ./src/$file }
+foreach file $dofiles { project addfile ./src/$file }
+project calculateorder
+
+foreach file $files { vlog ./src/$file }
+vsim work.$1 -wlf ./src/$1.wlf
+
+if { [file exists ./src/$1.do] == 0 } {
+	generate_do $1
 	project addfile ./src/$1.do
 } elseif {[file exists ./src/$1.do] == 1 && $dofiles == ""} {
 	do ./src/$1.do
